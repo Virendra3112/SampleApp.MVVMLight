eb579262026b50a9357d13fd05228c9e69631d92
Virendra <virendra.thakur31@gmail.com>
2021-06-26 10:55:38 +0530

    ViewmodelLocator updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/ViewModelLocator.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/ViewModelLocator.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GalaSoft.MvvmLight.Ioc;
+using SampleApp.MVVMLight.ViewModels;
 
 namespace SampleApp.MVVMLight.Helpers
 {
     public class ViewModelLocator
     {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<DasboardViewModel>();
+        }
+
+        public DasboardViewModel DasboardViewModel
+        {
+            get
+            {
+                if (!SimpleIoc.Default.IsRegistered<DasboardViewModel>())
+                {
+                    SimpleIoc.Default.Register<DasboardViewModel>();
+                }
+                return SimpleIoc.Default.GetInstance<DasboardViewModel>();
+            }
+        }
     }
 }
