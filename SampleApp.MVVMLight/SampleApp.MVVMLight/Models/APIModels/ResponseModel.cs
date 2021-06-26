7d20e7f26992f697ba1ab61253c50fac84084cea
Virendra <virendra.thakur31@gmail.com>
2021-06-27 00:11:46 +0530

    ResponseModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/APIModels/ResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace SampleApp.MVVMLight.Models.APIModels
+{
+    public class ResponseModel<T>
+    {
+        public HttpStatusCode Status { get; set; }
+
+        public bool IsSuccess
+        {
+            get
+            {
+                return Status == HttpStatusCode.OK || Status == HttpStatusCode.Created;
+            }
+
+        }
+        public T Result { get; set; }
+    }
+}
