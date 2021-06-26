d587be62547b1e1536c8707b30bc1cc4a18e4bcb
Virendra <virendra.thakur31@gmail.com>
2021-06-27 00:14:28 +0530

    HttpService added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Services/Implementation/HttpService.cs
@@ -0,0 +1,18 @@
+﻿using SampleApp.MVVMLight.Models.APIModels;
+using SampleApp.MVVMLight.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.MVVMLight.Services.Implementation
+{
+    public class HttpService : IHttpService
+    {
+        public Task<ResponseModel<string>> SendRequestAsync(HttpMethod method, string url, string content = null, int timeout = 60)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
