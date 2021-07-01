ce745416afb2d31d506cf986828ee8b2e616a082
Virendra <virendra.thakur31@gmail.com>
2021-07-01 10:55:18 +0530

    locator updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/Locator.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/Locator.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Autofac;
 
 namespace SampleApp.MVVMLight.Helpers
 {
-    class Locator
+    public class Locator
     {
+        public static Autofac.IContainer Container { get; set; }
+
+
+        protected static void RegisterCommon(ContainerBuilder builder)
+        {
+            RegisterServices(builder);
+        }
+
+
+        private static void RegisterServices(ContainerBuilder containerBuilder)
+        {
+            // containerBuilder.RegisterType<UserService>().As<IUserService>().InstancePerLifetimeScope();
+
+        }
     }
 }
