e99044329d0ed192341ef42af0dd6607e129fb7f
Virendra <virendra.thakur31@gmail.com>
2021-07-02 19:29:55 +0530

    MainActivity updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight.Android/MainActivity.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/MainActivity.cs
@@ -1,9 +1,8 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using SampleApp.MVVMLight.Droid.Helpers;
 
 namespace SampleApp.MVVMLight.Droid
 {
@@ -16,6 +15,9 @@ namespace SampleApp.MVVMLight.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            AndroidBootstrapper.Initialize();
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
