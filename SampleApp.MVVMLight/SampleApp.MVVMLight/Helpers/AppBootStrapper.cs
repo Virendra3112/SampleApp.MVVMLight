d9a8bc63c14f499cbefad6eb5a1c2b718b2ae869
Virendra <virendra.thakur31@gmail.com>
2021-07-02 19:09:44 +0530

    AppBootStrapper updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/AppBootStrapper.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/AppBootStrapper.cs
@@ -1,6 +1,29 @@
-﻿namespace SampleApp.MVVMLight.Helpers
+﻿using Autofac;
+using SampleApp.MVVMLight.ViewModels;
+
+namespace SampleApp.MVVMLight.Helpers
 {
-    public class AppBootStrapper
+    public class AppBootStrapper : Locator
     {
+        protected static void Init(ContainerBuilder builder)
+        {
+            Locator.RegisterCommon(builder);
+            RegisterServices(builder);
+            RegisterViewModel(builder);
+        }
+
+        /// <summary>
+        /// Registers the services.
+        /// </summary>
+        private static void RegisterServices(ContainerBuilder builder)
+        {
+
+        }
+
+        private static void RegisterViewModel(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(BaseViewModel).Assembly).InstancePerLifetimeScope().AsImplementedInterfaces().AsSelf().AssignableTo<BaseViewModel>();
+        }
+
     }
 }
