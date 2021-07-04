2af28745aff95eb500a1cea8f1d7b8bc2baa0b1a
Virendra <virendra.thakur31@gmail.com>
2021-07-04 12:02:35 +0530

    INavigationService added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Services/Interface/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleApp.MVVMLight.Services.Interface
+{
+    public interface INavigationService
+    {
+        string CurrentPageKey { get; }
+        void GoBack();
+        void NavigateTo(string pageKey);
+        void NavigateTo(string pageKey, object parameter);
+        void PopToRoot();
+        void HideDrawerMenu();
+    }
+}
