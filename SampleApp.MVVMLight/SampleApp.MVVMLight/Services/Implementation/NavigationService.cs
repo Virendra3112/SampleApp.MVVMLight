085432870f03874865daab113ba4b64accc342bf
Virendra <virendra.thakur31@gmail.com>
2021-07-04 12:04:47 +0530

    NavigationService added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Services/Implementation/NavigationService.cs
@@ -0,0 +1,37 @@
+﻿using SampleApp.MVVMLight.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Services.Implementation
+{
+    public class NavigationService : INavigationService
+    {
+        public string CurrentPageKey => throw new NotImplementedException();
+
+        public void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HideDrawerMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(string pageKey, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopToRoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
