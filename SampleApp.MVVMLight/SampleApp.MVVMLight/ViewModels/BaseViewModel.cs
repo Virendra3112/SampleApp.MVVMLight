c248a7034c081aff778d2e03ce3823bd8085f5d3
Virendra <virendra.thakur31@gmail.com>
2021-07-04 11:57:53 +0530

    BaseViewModel updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/BaseViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/BaseViewModel.cs
@@ -1,10 +1,21 @@
-﻿namespace SampleApp.MVVMLight.ViewModels
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SampleApp.MVVMLight.ViewModels
 {
-    public class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
         public BaseViewModel()
         {
 
         }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
     }
 }
