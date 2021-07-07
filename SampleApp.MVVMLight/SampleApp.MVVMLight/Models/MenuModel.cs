e1e996b946c44e39721e41190c26229560dc8838
Virendra <virendra.thakur31@gmail.com>
2021-07-07 11:13:52 +0530

    MenuModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/MenuModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Models
+{
+    public class MenuModel
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string PageName { get; set; }
+    }
+}
