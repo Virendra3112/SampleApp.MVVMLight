efdb9ffbf7ee52a8a43bbf6287417d9e27559155
Virendra <virendra.thakur31@gmail.com>
2021-07-08 11:22:29 +0530

    DasboardViewModel updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/DasboardViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/DasboardViewModel.cs
@@ -1,10 +1,37 @@
-﻿namespace SampleApp.MVVMLight.ViewModels
+﻿using SampleApp.MVVMLight.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SampleApp.MVVMLight.ViewModels
 {
     public class DasboardViewModel : BaseViewModel
     {
+
+        private ObservableCollection<MenuModel> _categoryList;
+
+        public ObservableCollection<MenuModel> CategoryList
+        {
+            get { return _categoryList; }
+            set { _categoryList = value; OnPropertyChanged(); }
+        }
+        public ICommand MenuItemCommand { get; set; }
         public DasboardViewModel()
         {
 
+            MenuItemCommand = new Command(MenuSelected);
+
+            CategoryList = new ObservableCollection<MenuModel>();
+
+
+            CategoryList.Add(new MenuModel { PageName = "Notification View", Icon = "icon.png" });
+
+        }
+
+        private void MenuSelected(object obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
