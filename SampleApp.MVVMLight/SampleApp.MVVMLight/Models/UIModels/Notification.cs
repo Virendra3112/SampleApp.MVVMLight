61e80271d3847ecf77aeff68ac75d70d8a8aa4dd
Virendra <virendra.thakur31@gmail.com>
2021-07-12 11:10:22 +0530

    Notification model added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/UIModels/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SampleApp.MVVMLight.Models.UIModels
+{
+    public class Notification
+    {
+        public long NotificationId { get; set; }
+        public string Title { get; set; }
+        public long Body { get; set; }
+        public DateTime ReceivedDateTime { get; set; }
+        public string ImageURL { get; set; }
+        public bool IsRead { get; set; }
+        public bool IsFavourite { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsSelected { get; set; }
+        public string ExtraParameterFirst { get; set; }
+        public string ExtraParameterSeccond { get; set; }
+    }
+}
