53525992cd4ac4ecf2fc9debb84a66199c5fbda4
Virendra <virendra.thakur31@gmail.com>
2021-07-13 22:06:31 +0530

    NotificationViewModel updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/NotificationViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/NotificationViewModel.cs
@@ -1,10 +1,64 @@
-﻿namespace SampleApp.MVVMLight.ViewModels
+﻿using SampleApp.MVVMLight.Models.UIModels;
+using System;
+using System.Collections.ObjectModel;
+
+namespace SampleApp.MVVMLight.ViewModels
 {
     public class NotificationViewModel : BaseViewModel
     {
+        private ObservableCollection<Notification> _detailsList;
+        public ObservableCollection<Notification> DetailsList
+        {
+            get
+            {
+                return _detailsList;
+            }
+            set
+            {
+                if (_detailsList != value)
+                {
+                    _detailsList = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public NotificationViewModel()
         {
 
+        }
+
+        public void GetData()
+        {
+            try
+            {
+                DetailsList = new ObservableCollection<Notification>();
+                DetailsList.Add(new Notification
+                {
+                    Body = "Test body",
+                    Title = "Test Titel"
+                }); DetailsList.Add(new Notification
+                {
+                    Body = "Test body",
+                    Title = "Test Titel"
+                }); DetailsList.Add(new Notification
+                {
+                    Body = "Test body",
+                    Title = "Test Titel"
+                }); DetailsList.Add(new Notification
+                {
+                    Body = "Test body",
+                    Title = "Test Titel"
+                }); DetailsList.Add(new Notification
+                {
+                    Body = "Test body",
+                    Title = "Test Titel"
+                });
+
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
