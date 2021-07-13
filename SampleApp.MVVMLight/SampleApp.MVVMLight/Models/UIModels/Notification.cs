107fcd51f541518fdb36c92957a49cd6ec966c0f
Virendra <virendra.thakur31@gmail.com>
2021-07-13 22:05:39 +0530

    Notification model updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/Models/UIModels/Notification.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/UIModels/Notification.cs
@@ -6,7 +6,7 @@ namespace SampleApp.MVVMLight.Models.UIModels
     {
         public long NotificationId { get; set; }
         public string Title { get; set; }
-        public long Body { get; set; }
+        public string Body { get; set; }
         public DateTime ReceivedDateTime { get; set; }
         public string ImageURL { get; set; }
         public bool IsRead { get; set; }
