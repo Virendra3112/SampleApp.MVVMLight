0447ca8835d4433a8c9cf10bfd4b28c6ee669907
Virendra <virendra.thakur31@gmail.com>
2021-07-19 11:40:24 +0530

    BaseViewModel updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/BaseViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/BaseViewModel.cs
@@ -1,5 +1,7 @@
-﻿using CommonServiceLocator;
+﻿using Acr.UserDialogs;
+using CommonServiceLocator;
 using SampleApp.MVVMLight.Services.Interface;
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -9,9 +11,62 @@ namespace SampleApp.MVVMLight.ViewModels
     {
         protected readonly INavigationService NavigationService;
 
+        #region Properties
+        //public bool IsConnected
+        //{
+        //    get { return CrossConnectivity.Current.IsConnected; }
+        //}
+
+        bool isBusy;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+            set
+            {
+                isBusy = value;
+                ShowLoading(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isNetworkAvailable;
+
+        public bool IsNetworkAvailable
+        {
+            get => _isNetworkAvailable;
+            set
+            {
+                _isNetworkAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
         public BaseViewModel()
         {
             NavigationService = ServiceLocator.Current.GetInstance<INavigationService>();
+        }
+
+
+        private void ShowLoading(bool value)
+        {
+            try
+            {
+
+                if (value)
+                    UserDialogs.Instance.ShowLoading("Loading...", MaskType.Black);
+
+                else
+                    UserDialogs.Instance.HideLoading();
+            }
+            catch (Exception ex)
+            {
+            }
         }
 
 
