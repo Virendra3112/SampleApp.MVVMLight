9cf4674e178296647371b9c23a1c2067831a4f71
Virendra <virendra.thakur31@gmail.com>
2021-07-20 12:11:13 +0530

    App.cs updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/App.xaml.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/App.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Autofac.Extras.CommonServiceLocator;
+using Com.OneSignal;
+using Com.OneSignal.Abstractions;
 using CommonServiceLocator;
 using SampleApp.MVVMLight.Helpers;
 using SampleApp.MVVMLight.Views;
 using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace SampleApp.MVVMLight
@@ -35,6 +38,19 @@ namespace SampleApp.MVVMLight
                 MasterDetail.MasterBehavior = MasterBehavior.Popover;
                 MasterDetail.IsPresented = false;
                 MainPage = MasterDetail;
+
+                //Remove this method to stop OneSignal Debugging  
+                OneSignal.Current.SetLogLevel(LOG_LEVEL.VERBOSE, LOG_LEVEL.NONE);
+
+                OneSignal.Current.StartInit("YOUR_ONESIGNAL_APP_ID") //todo add AppId here
+                .Settings(new Dictionary<string, bool>() {
+                                { IOSSettings.kOSSettingsKeyAutoPrompt, false },
+                                { IOSSettings.kOSSettingsKeyInAppLaunchURL, false } })
+                .InFocusDisplaying(OSInFocusDisplayOption.Notification)
+                .EndInit();
+
+                // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+                OneSignal.Current.RegisterForPushNotifications();
             }
             catch (Exception ex)
             {
