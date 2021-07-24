80289c2e853d101aa63fc8ad78cd5fea3b801f68
Virendra <virendra.thakur31@gmail.com>
2021-07-24 12:09:43 +0530

    updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/Views/CustomPopupView.xaml.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Views/CustomPopupView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Rg.Plugins.Popup.Pages;
+﻿using CommonServiceLocator;
+using Rg.Plugins.Popup.Pages;
+using SampleApp.MVVMLight.ViewModels;
 using Xamarin.Forms.Xaml;
 
 namespace SampleApp.MVVMLight.Views
@@ -6,9 +8,17 @@ namespace SampleApp.MVVMLight.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CustomPopupView : PopupPage
     {
+        private CustomPopupViewModel CustomPopupViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<CustomPopupViewModel>();
+            }
+        }
         public CustomPopupView()
         {
             InitializeComponent();
+            BindingContext = CustomPopupViewModel;
         }
     }
 }
