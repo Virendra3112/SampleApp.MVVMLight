1d3d893c76a4ea0a23389811a40403698965349a
Virendra <virendra.thakur31@gmail.com>
2021-07-25 19:56:27 +0530

    CustomPopupViewModel updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/CustomPopupViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/CustomPopupViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SampleApp.MVVMLight.ViewModels
 {
     public class CustomPopupViewModel : BaseViewModel
     {
+        public ICommand ConfirmCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+        public CustomPopupViewModel()
+        {
+            ConfirmCommand = new Command(OnConfirmClicked);
+            CancelCommand = new Command(OnCancelClicked);
+        }
+
+        private void OnConfirmClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        private void OnCancelClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
