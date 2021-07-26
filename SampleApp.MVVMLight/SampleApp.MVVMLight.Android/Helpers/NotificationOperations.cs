e952e90f0c34a8821c7698b58f94689625076d8c
Virendra <virendra.thakur31@gmail.com>
2021-07-26 18:39:41 +0530

    NotificationOperations added for android.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/Helpers/NotificationOperations.cs
@@ -0,0 +1,23 @@
+﻿using SampleApp.MVVMLight.Helpers;
+using System;
+
+namespace SampleApp.MVVMLight.Droid.Helpers
+{
+    public class NotificationOperations : INotificationOperations
+    {
+        public void ClearNotification(long notificationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetNotificationBadge(long badge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowNotification(string title, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
