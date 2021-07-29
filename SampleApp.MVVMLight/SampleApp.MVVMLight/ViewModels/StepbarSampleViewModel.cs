0c97910a53ee6ad16062ea69172b0d23d5e8dbec
Virendra <virendra.thakur31@gmail.com>
2021-07-29 20:34:41 +0530

    StepbarSampleViewModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/StepbarSampleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.MVVMLight.ViewModels
+{
+    public class StepbarSampleViewModel : BaseViewModel
+    {
+        public StepbarSampleViewModel()
+        {
+
+        }
+    }
+}
