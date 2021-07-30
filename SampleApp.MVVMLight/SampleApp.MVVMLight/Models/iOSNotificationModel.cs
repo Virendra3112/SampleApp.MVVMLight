eea67f4eb9a4a494c9f4c6179477d0e2aa3026d1
Virendra <virendra.thakur31@gmail.com>
2021-07-30 19:51:42 +0530

    iOSNotificationModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/iOSNotificationModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SampleApp.MVVMLight.Models
+{
+    public class iOSNotificationModel
+    {
+        public int ApnsCollapseId { get; set; }
+
+        public Alert alert { get; set; }
+        public long Badge { get; set; }
+        public string sound { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+    public partial class Alert
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+}
