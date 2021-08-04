e6e2d369087a594e9a83f426f90ce4df85cd9c0d
Virendra <virendra.thakur31@gmail.com>
2021-08-04 23:05:56 +0530

    BaseDependencyImplementation added for android.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/Helpers/BaseDependencyImplementation.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using System;
+using Xamarin.Forms;
+
+namespace SampleApp.MVVMLight.Droid.Helpers
+{
+    public class BaseDependencyImplementation : Object
+    {
+        public Activity GetActivity()
+        {
+            var activity = (Activity)Forms.Context;
+            return activity;
+        }
+    }
+}
