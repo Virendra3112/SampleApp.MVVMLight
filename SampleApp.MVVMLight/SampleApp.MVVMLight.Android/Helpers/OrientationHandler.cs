a6168daa348101b6747f148de67e57d438576d74
Virendra <virendra.thakur31@gmail.com>
2021-08-04 22:09:16 +0530

    OrientationHandler added for android.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/Helpers/OrientationHandler.cs
@@ -0,0 +1,18 @@
+﻿using SampleApp.MVVMLight.Services.Interface;
+using System;
+
+namespace SampleApp.MVVMLight.Droid.Helpers
+{
+    public class OrientationHandler : IOrientationHandler
+    {
+        public void ForceLandscape()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ForcePortrait()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
