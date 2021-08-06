fbe4366934e0e4e1d5f43c7dc2d1d365bed10d28
Virendra <virendra.thakur31@gmail.com>
2021-08-06 23:05:37 +0530

    ValidatableObject added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/ValidatableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Helpers
+{
+    public class ValidatableObject<T> : IValidatable<T>
+    {
+        public List<IValidationRule<T>> Validations => throw new NotImplementedException();
+
+        public List<string> Errors { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsValid { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
