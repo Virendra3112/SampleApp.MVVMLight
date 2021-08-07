9cf009e87e6577a68705f24e62e7b6c69ec69928
Virendra <virendra.thakur31@gmail.com>
2021-08-07 13:13:54 +0530

    IsValidEmailRule added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/IsValidEmailRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Helpers
+{
+    public class IsValidEmailRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool Check(T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
