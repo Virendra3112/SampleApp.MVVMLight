4a8678f05b6f59448c47f425d5a3839da81c8ece
Virendra <virendra.thakur31@gmail.com>
2021-08-07 12:04:31 +0530

    ValidatableObject updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/ValidatableObject.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Helpers/ValidatableObject.cs
@@ -1,22 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
+using System.Linq;
 
 namespace SampleApp.MVVMLight.Helpers
 {
     public class ValidatableObject<T> : IValidatable<T>
     {
-        public List<IValidationRule<T>> Validations => throw new NotImplementedException();
-
-        public List<string> Errors { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsValid { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public bool Validate()
+        public List<IValidationRule<T>> Validations { get; } = new List<IValidationRule<T>>();
+
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public bool CleanOnChange { get; set; } = true;
+
+        T _value;
+        public T Value
         {
-            throw new NotImplementedException();
+            get => _value;
+            set
+            {
+                _value = value;
+
+                if (CleanOnChange)
+                    IsValid = true;
+            }
+        }
+
+        public bool IsValid { get; set; } = true;
+
+        public virtual bool Validate()
+        {
+            Errors.Clear();
+
+            IEnumerable<string> errors = Validations.Where(v => !v.Check(Value))
+                .Select(v => v.ValidationMessage);
+
+            Errors = errors.ToList();
+            IsValid = !Errors.Any();
+
+            return this.IsValid;
+        }
+        public override string ToString()
+        {
+            return $"{Value}";
         }
     }
 }
