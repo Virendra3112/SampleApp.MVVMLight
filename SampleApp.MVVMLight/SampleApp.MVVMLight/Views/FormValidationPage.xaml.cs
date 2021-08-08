aee0cdc5e1861271c95674afb4d63768e03c3148
Virendra <virendra.thakur31@gmail.com>
2021-08-08 16:38:13 +0530

    FormValidationPage.xaml added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Views/FormValidationPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CommonServiceLocator;
+using SampleApp.MVVMLight.Helpers;
+using SampleApp.MVVMLight.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleApp.MVVMLight.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormValidationPage : ContentPage
+    {
+        private FormValidationPageModel FormValidationPageModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<FormValidationPageModel>();
+            }
+        }
+        public FormValidationPage()
+        {
+            InitializeComponent();
+            BindingContext = FormValidationPageModel;
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+        }
+    }
+}
