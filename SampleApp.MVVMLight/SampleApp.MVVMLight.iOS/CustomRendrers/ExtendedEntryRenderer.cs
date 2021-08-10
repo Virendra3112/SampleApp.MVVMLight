b3f361514229cf954c772fc409c1433a9100c23e
Virendra <virendra.thakur31@gmail.com>
2021-08-11 00:03:26 +0530

    ExtendedEntryRenderer added for iOS.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.iOS/CustomRendrers/ExtendedEntryRenderer.cs
@@ -0,0 +1,38 @@
+﻿using SampleApp.MVVMLight.CustomControls;
+using SampleApp.MVVMLight.iOS.CustomRendrers;
+using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryRenderer))]
+namespace SampleApp.MVVMLight.iOS.CustomRendrers
+{
+    public class ExtendedEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (Control == null || this.Element == null) return;
+
+			if (e.PropertyName == ExtendedEntry.IsBorderErrorVisibleProperty.PropertyName)
+			{
+				if (((ExtendedEntry)this.Element).IsBorderErrorVisible)
+				{
+					this.Control.Layer.BorderColor = ((ExtendedEntry)this.Element).BorderErrorColor.ToCGColor();
+					this.Control.Layer.BorderWidth = new nfloat(0.8);
+					this.Control.Layer.CornerRadius = 5;
+				}
+				else
+				{
+					this.Control.Layer.BorderColor = UIColor.LightGray.CGColor;
+					this.Control.Layer.CornerRadius = 5;
+					this.Control.Layer.BorderWidth = new nfloat(0.8);
+				}
+
+			}
+		}
+
+	}
+}
--- SampleApp.MVVMLight/SampleApp.MVVMLight.iOS/Helpers/NotificationOperations.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.iOS/Helpers/NotificationOperations.cs
@@ -1,6 +1,11 @@
-﻿using SampleApp.MVVMLight.Helpers;
+﻿using Foundation;
+using Newtonsoft.Json;
+using SampleApp.MVVMLight.Helpers;
+using SampleApp.MVVMLight.Models;
 using System;
+using System.Collections.Generic;
 using UIKit;
+using UserNotifications;
 
 namespace SampleApp.MVVMLight.iOS.Helpers
 {
@@ -8,7 +13,41 @@ namespace SampleApp.MVVMLight.iOS.Helpers
     {
         public void ClearNotification(long notificationId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<string> ids = new List<string>();
+
+                UNUserNotificationCenter.Current.GetDeliveredNotifications(completionHandler: (UNNotification[] t) =>
+                {
+
+                    foreach (UNNotification item in t)
+                    {
+                        UNNotificationRequest uNNotificationRequest = item.Request;
+
+                        NSError nSError;
+
+                        var data = NSJsonSerialization.Serialize((NSDictionary)uNNotificationRequest.Content.UserInfo["aps"], NSJsonWritingOptions.PrettyPrinted, out nSError);
+
+                        var jsonstring = NSString.FromData(data, NSStringEncoding.UTF8).ToString();
+
+                        var model = JsonConvert.DeserializeObject<iOSNotificationModel>(jsonstring);
+
+                        if (notificationId == model.ApnsCollapseId)
+                        {
+                            ids.Add(uNNotificationRequest.Identifier);
+                        }
+                    }
+
+                    //Clear the notification from notification center
+
+                    UNUserNotificationCenter.Current.RemoveDeliveredNotifications(ids.ToArray());
+                });
+
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         public void SetNotificationBadge(long badge)
