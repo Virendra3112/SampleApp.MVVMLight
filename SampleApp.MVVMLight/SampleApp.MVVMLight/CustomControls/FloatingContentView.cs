d20287612063d55860e956c6a52aa2ce905010de
Virendra <virendra.thakur31@gmail.com>
2021-08-12 10:04:38 +0530

    FloatingContentView updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/CustomControls/FloatingContentView.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/CustomControls/FloatingContentView.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics;
+using Xamarin.Forms;
 
 namespace SampleApp.MVVMLight.CustomControls
 {
-    class FloatingContentView
+    public class FloatingContentView : ContentView
     {
+
+        private double x, y;
+        public FloatingContentView()
+        {
+            var panGesture = new PanGestureRecognizer();
+
+            panGesture.PanUpdated += OnPanUpdated;
+            this.GestureRecognizers.Add(panGesture);
+        }
+
+        private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+
+                    Debug.WriteLine("DEBUG: start dragging");
+                    break;
+                case GestureStatus.Running:
+
+                    Content.TranslationX = x + e.TotalX;
+                    Content.TranslationY = y + e.TotalY;
+
+                    break;
+                case GestureStatus.Completed:
+
+                    x = Content.TranslationX;
+                    y = Content.TranslationY;
+
+                    Debug.WriteLine("DEBUG: drag ended");
+                    break;
+            }
+        }
     }
 }
