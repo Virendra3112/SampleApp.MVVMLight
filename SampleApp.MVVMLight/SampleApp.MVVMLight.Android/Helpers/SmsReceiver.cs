536ac143047d6e9add5919036593dc62b48dc0af
Virendra <virendra.thakur31@gmail.com>
2021-08-14 15:20:38 +0530

    SmsReceiver added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/Helpers/SmsReceiver.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Droid.Helpers
+{
+    public class SmsReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
