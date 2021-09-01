12e677cd0f73542eef297521f25fddc33a6d5384
Virendra <virendra.thakur31@gmail.com>
2021-09-01 13:07:12 +0530

    BottomSheetDemoViewModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/BottomSheetDemoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SampleApp.MVVMLight.ViewModels
+{
+    public class BottomSheetDemoViewModel : BaseViewModel
+    {
+        public ICommand OpenPopupCommand { get; set; }
+
+        public BottomSheetDemoViewModel()
+        {
+            OpenPopupCommand = new Command(OnOpenPopup);
+
+        }
+
+        private async void OnOpenPopup(object obj)        
+        {
+            //await Sheet.OpenSheet();
+
+        }
+    }
+}
