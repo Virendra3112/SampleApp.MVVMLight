4b4ff3915d0627bd342280b27f461016b43ecbe2
Virendra <virendra.thakur31@gmail.com>
2021-09-02 21:35:26 +0530

    BottomSheetDemoView added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Views/BottomSheetDemoView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CommonServiceLocator;
+using SampleApp.MVVMLight.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleApp.MVVMLight.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BottomSheetDemoView : ContentPage
+    {
+        private BottomSheetDemoViewModel BottomSheetDemoViewVM
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<BottomSheetDemoViewModel>();
+            }
+        }
+        public BottomSheetDemoView()
+        {
+            InitializeComponent();
+            BindingContext = BottomSheetDemoViewVM;
+        }
+
+        private async void Button_Clicked(object sender, System.EventArgs e)
+        {
+           //await Application.Current.MainPage.Navigation.PushModalAsync(new OTPPage());
+                //ShowPopup(new CustomPopUp());
+
+            await Sheet.OpenSheet();
+        }
+    }
+}
