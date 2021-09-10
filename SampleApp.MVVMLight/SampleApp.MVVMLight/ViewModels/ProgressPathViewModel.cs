6c5abf66d7a71a834096ace1fccfb76ca09f46a8
Virendra <virendra.thakur31@gmail.com>
2021-09-10 11:13:08 +0530

    ProgressPathViewModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/ProgressPathViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.MVVMLight.ViewModels
+{
+    public class ProgressPathViewModel : BaseViewModel
+    {
+    }
+}
