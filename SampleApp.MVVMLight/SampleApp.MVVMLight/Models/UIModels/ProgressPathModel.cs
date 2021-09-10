11c5e283b00100ba356da575b11289c6ad6fdb9e
Virendra <virendra.thakur31@gmail.com>
2021-09-10 11:17:55 +0530

    ProgressPathModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/UIModels/ProgressPathModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleApp.MVVMLight.Models.UIModels
+{
+    public class ProgressPathModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
