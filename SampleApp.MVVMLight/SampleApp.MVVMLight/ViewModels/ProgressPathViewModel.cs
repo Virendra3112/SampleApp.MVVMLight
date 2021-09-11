517a6c15747b174033ec15298cffa552c4398717
Virendra <virendra.thakur31@gmail.com>
2021-09-11 13:29:53 +0530

    ProgressPathViewModel updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/ProgressPathViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/ProgressPathViewModel.cs
@@ -1,10 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SampleApp.MVVMLight.Models.UIModels;
+using System;
+using System.Collections.ObjectModel;
 
 namespace SampleApp.MVVMLight.ViewModels
 {
     public class ProgressPathViewModel : BaseViewModel
     {
+
+        private ObservableCollection<ProgressPathModel> _groupedItems;
+        public ObservableCollection<ProgressPathModel> GroupedItems
+        {
+            get { return _groupedItems; }
+            set
+            {
+                _groupedItems = value; OnPropertyChanged();
+            }
+        }
+
+        public ProgressPathViewModel()
+        {
+            GroupedItems = new ObservableCollection<ProgressPathModel>();
+
+            GetData();
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                GroupedItems.Add(new ProgressPathModel
+                {
+                    Id = 1,
+                    Name = "Test Item 1",
+                    ImageOrientation = "Horizontal",
+                    ImagePath = "resource://SampleApp.MVVMLight.Resources.path.svg"
+                });
+
+                GroupedItems.Add(new ProgressPathModel
+                {
+                    Id = 2,
+                    Name = "Test Item 2",
+                    ImageOrientation = "Vertical",
+                    ImagePath = "resource://SampleApp.MVVMLight.Resources.pathvertical.svg"
+                });
+                
+                GroupedItems.Add(new ProgressPathModel
+                {
+                    Id = 3,
+                    Name = "Test Item 3",
+                    ImageOrientation = "Horizontal",
+                    ImagePath = "resource://SampleApp.MVVMLight.Resources.pathvertical.svg"
+                });
+                
+                GroupedItems.Add(new ProgressPathModel
+                {
+                    Id = 4,
+                    Name = "Test Item 4",
+                    ImageOrientation = "Vertical",
+                    ImagePath = "resource://SampleApp.MVVMLight.Resources.pathvertical.svg"
+                });
+
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
