d0f7cb7fe9b63a468db58de4370e9e50dd652453
Virendra <virendra.thakur31@gmail.com>
2021-09-13 11:14:34 +0530

    ProgressPathVM updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/Models/UIModels/ProgressPathModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/UIModels/ProgressPathModel.cs
@@ -7,5 +7,6 @@
         public string Name { get; set; }
         public string ImageOrientation { get; set; }
         public string ImagePath { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
