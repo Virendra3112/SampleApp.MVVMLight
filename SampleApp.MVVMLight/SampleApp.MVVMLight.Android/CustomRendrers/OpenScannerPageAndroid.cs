23715205555074243f0c8d4c2c12402f44286584
Virendra <virendra.thakur31@gmail.com>
2021-09-20 11:11:15 +0530

    OpenScannerPageAndroid added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/CustomRendrers/OpenScannerPageAndroid.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using SampleApp.MVVMLight.Droid.CustomRendrers;
+using SampleApp.MVVMLight.Helpers;
+using System;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(OpenScannerPageAndroid))]
+namespace SampleApp.MVVMLight.Droid.CustomRendrers
+{
+    public class OpenScannerPageAndroid : IOpenScannerPage
+    {
+        public void OpenScanner()
+        {
+            try
+            {
+                //Android.App.Application.Context.StartActivity(typeof(FragmentActivity));
+
+                Intent intent = new Intent();
+                intent.SetClass(Android.App.Application.Context, typeof(FragmentActivity));
+                intent.AddFlags(ActivityFlags.NewTask);
+                Android.App.Application.Context.StartActivity(intent);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
