af671b8c16d87cf440919a462696ed3f2d7b8896
Virendra <virendra.thakur31@gmail.com>
2021-09-21 11:45:19 +0530

    OpenScannerPageAndroid updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight.Android/CustomRendrers/OpenScannerPageAndroid.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/CustomRendrers/OpenScannerPageAndroid.cs
@@ -2,7 +2,9 @@
 using SampleApp.MVVMLight.Droid.CustomRendrers;
 using SampleApp.MVVMLight.Helpers;
 using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using ZXing.Mobile;
 
 [assembly: Dependency(typeof(OpenScannerPageAndroid))]
 namespace SampleApp.MVVMLight.Droid.CustomRendrers
@@ -25,6 +27,21 @@ namespace SampleApp.MVVMLight.Droid.CustomRendrers
             {
 
             }
+        }
+
+        public async Task<string> ScanAsync()
+        {
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait",
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult.Text;
         }
     }
 }
