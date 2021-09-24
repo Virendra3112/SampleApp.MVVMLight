3b4e19f7fde632b273efd96dad0f7c2d3a7e4b49
Virendra <virendra.thakur31@gmail.com>
2021-09-24 19:22:32 +0530

    QRScannerViewModel updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/QRScannerViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/QRScannerViewModel.cs
@@ -1,7 +1,37 @@
-﻿namespace SampleApp.MVVMLight.ViewModels
+﻿using Acr.UserDialogs;
+using Xamarin.Forms;
+
+namespace SampleApp.MVVMLight.ViewModels
 {
     public class QRScannerViewModel : BaseViewModel
     {
+        private bool isScanning = true;
+
+        public bool IsScanning
+        {
+            get => isScanning;
+            set
+            {
+                isScanning = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command<string> ResultCommand => new Command<string>(async (result) =>
+        {
+            if (IsScanning)
+            {
+                IsScanning = false;
+
+                UserDialogs.Instance.Alert("QR scanned: " + result, "OK");
+
+                //await DisplayAlert("QR scanned", result, "Ok");
+
+                IsScanning = true;
+            }
+        });
+
+
         public QRScannerViewModel()
         {
 
