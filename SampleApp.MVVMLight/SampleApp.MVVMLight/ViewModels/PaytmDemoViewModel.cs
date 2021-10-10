f488148f5a3938c103a3e2414aff29cb79788411
Virendra <virendra.thakur31@gmail.com>
2021-10-10 21:52:40 +0530

    commit.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/PaytmDemoViewModel.cs
@@ -0,0 +1,34 @@
+﻿using AllInOneSDK;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SampleApp.MVVMLight.ViewModels
+{
+    public class PaytmDemoViewModel : BaseViewModel, PaymentCallback
+    {
+        public ICommand PayNowCommand { get; set; }
+
+        public PaytmDemoViewModel()
+        {
+            PayNowCommand = new Command(OnPay);
+        }
+
+        public void error(string errorMessage)
+        {
+            AllInOnePlugin.DestroyInstance();
+        }
+
+        public void success(Dictionary<string, object> dictionary)
+        {
+            AllInOnePlugin.DestroyInstance();
+        }
+
+        public void OnPay(object obj)
+        {
+            //AllInOnePlugin.startTransaction(orderId, mid, txnToken, amount, callbackurl, isStaging, restrictAppInvoke, this); // this -> PaymentCallback
+
+            AllInOnePlugin.startTransaction("", "12345", "", "", "", false, false, this); // this -> PaymentCallback
+        }
+    }
+}
