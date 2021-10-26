ac8d4e80cd339bba256380f39624c2dc5f740c31
Virendra <virendra.thakur31@gmail.com>
2021-10-26 21:45:02 +0530

    commit

--- SampleApp.MVVMLight/SampleApp.MVVMLight/App.xaml.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/App.xaml.cs
@@ -12,7 +12,6 @@ namespace SampleApp.MVVMLight
 {
     public partial class App : Application
     {
-
         public static MasterDetailPage MasterDetail { get; set; }
         public static NavigationPage Navigation { get; set; }
         public App()
@@ -24,9 +23,7 @@ namespace SampleApp.MVVMLight
                 if (!ServiceLocator.IsLocationProviderSet)
                     ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(Locator.Container));
 
-
                 //MainPage = new NavigationPage(new DasboardView());
-
                 MasterDetail = new MasterDetailPage();
                 MasterDetail.Master = new NavigationDrawerPage() { Title = " " };
                 Navigation = new NavigationPage(new DasboardView())
@@ -40,14 +37,14 @@ namespace SampleApp.MVVMLight
                 MainPage = MasterDetail;
 
                 //Remove this method to stop OneSignal Debugging  
-                OneSignal.Current.SetLogLevel(LOG_LEVEL.VERBOSE, LOG_LEVEL.NONE);
+                //OneSignal.Current.SetLogLevel(LOG_LEVEL.VERBOSE, LOG_LEVEL.NONE);
 
-                OneSignal.Current.StartInit("YOUR_ONESIGNAL_APP_ID") //todo add AppId here
-                .Settings(new Dictionary<string, bool>() {
-                                { IOSSettings.kOSSettingsKeyAutoPrompt, false },
-                                { IOSSettings.kOSSettingsKeyInAppLaunchURL, false } })
-                .InFocusDisplaying(OSInFocusDisplayOption.Notification)
-                .EndInit();
+                //OneSignal.Current.StartInit("YOUR_ONESIGNAL_APP_ID") //todo add AppId here
+                //.Settings(new Dictionary<string, bool>() {
+                //                { IOSSettings.kOSSettingsKeyAutoPrompt, false },
+                //                { IOSSettings.kOSSettingsKeyInAppLaunchURL, false } })
+                //.InFocusDisplaying(OSInFocusDisplayOption.Notification)
+                //.EndInit();
 
                 // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
                 OneSignal.Current.RegisterForPushNotifications();
