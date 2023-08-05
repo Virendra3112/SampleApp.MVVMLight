fd37664695bb3bb21cb1e435cde0d78fe09fa20a
Virendra <virendra.thakur31@gmail.com>
2023-08-05 20:51:51 +0530

    commit

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/CustomRendrers/WifiConnectorAndroid.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SampleApp.MVVMLight.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Droid.CustomRendrers
+{
+    class WifiConnectorAndroid : IWifiConnector
+    {
+        public void ConnectToWifi(string ssid, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
