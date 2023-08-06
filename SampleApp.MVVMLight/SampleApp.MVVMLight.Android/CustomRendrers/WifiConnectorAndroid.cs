0e831927dca4867e5fd50969499b24c351841118
Virendra <virendra.thakur31@gmail.com>
2023-08-06 21:30:36 +0530

    WifiConnectorAndroid updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight.Android/CustomRendrers/WifiConnectorAndroid.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/CustomRendrers/WifiConnectorAndroid.cs
@@ -1,22 +1,43 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.Content;
+using Android.Net;
+using Android.Net.Wifi;
+using SampleApp.MVVMLight.Droid.CustomRendrers;
 using SampleApp.MVVMLight.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Xamarin.Forms;
 
+[assembly: Dependency(typeof(WifiConnectorAndroid))]
 namespace SampleApp.MVVMLight.Droid.CustomRendrers
 {
-    class WifiConnectorAndroid : IWifiConnector
+    public class WifiConnectorAndroid : IWifiConnector
     {
         public void ConnectToWifi(string ssid, string password)
         {
-            throw new NotImplementedException();
+            //var formattedSsid = $"\"{ssid}\"";
+            //var formattedPassword = $"\"{password}\"";
+
+            //var wifiConfig = new WifiConfiguration
+            //{
+            //    Ssid = formattedSsid,
+            //    PreSharedKey = formattedPassword
+            //};
+
+            //var wifiManager = (WifiManager)Android.App.Application.Context
+            //            .GetSystemService(Context.WifiService);
+
+            //var addNetwork = wifiManager.AddNetwork(wifiConfig);
+
+
+            WifiNetworkSpecifier wifiNetworkSpecifier = new WifiNetworkSpecifier.Builder()
+            .SetSsid(ssid)
+            .SetWpa2Passphrase(password)
+            .Build();
+            NetworkRequest networkRequest = new NetworkRequest.Builder()
+                        .AddTransportType(Android.Net.TransportType.Wifi)// NetworkCapabilities.TransportWifi)
+                        .SetNetworkSpecifier(wifiNetworkSpecifier)
+                        .Build();
+            ConnectivityManager connectivityManager =
+                (ConnectivityManager)Android.App.Application.Context.GetSystemService(Context.ConnectivityService);
+            connectivityManager.RequestNetwork(networkRequest, new ConnectivityManager.NetworkCallback());
         }
     }
 }
