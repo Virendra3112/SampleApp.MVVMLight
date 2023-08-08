a09128070b8c2bb12abf7bbc4755d50c25a43160
Virendra <virendra.thakur31@gmail.com>
2023-08-08 22:28:24 +0530

    Commit.

--- SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/ConnectToWiFiPageViewModel.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/ConnectToWiFiPageViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SampleApp.MVVMLight.ViewModels
 {
@@ -47,10 +49,18 @@ namespace SampleApp.MVVMLight.ViewModels
                 }
             }
         }
+
+        public ICommand WifiItemSelectedCommand { get; set; }
         public ConnectToWiFiPageViewModel()
         {
+            WifiItemSelectedCommand = new Command(OnItemSelected);
             //ConnectToWifi();
             GetAvailableNetworksAsync();
+        }
+
+        private void OnItemSelected(object obj)
+        {
+            throw new NotImplementedException();
         }
 
         private async Task GetAvailableNetworksAsync()
