2ba07f741fd09d31ee9d1b274e944be103422cf9
Virendra <virendra.thakur31@gmail.com>
2023-08-11 21:33:59 +0530

    NetworkCallback added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/Helpers/NetworkCallback.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Net;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Droid.Helpers
+{
+    public class NetworkCallback : ConnectivityManager.NetworkCallback
+    {
+    }
+}
