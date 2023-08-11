2313f6f51ccf9571a3f7d1dac8df2b6b38a1c580
Virendra <virendra.thakur31@gmail.com>
2023-08-11 21:35:03 +0530

    NetworkCallback updated.

--- SampleApp.MVVMLight/SampleApp.MVVMLight.Android/Helpers/NetworkCallback.cs
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/Helpers/NetworkCallback.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Net;
+using Android.Net.Wifi;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -14,5 +15,20 @@ namespace SampleApp.MVVMLight.Droid.Helpers
 {
     public class NetworkCallback : ConnectivityManager.NetworkCallback
     {
+        public NetworkCallback(int flags) //: base(flags)
+        {
+        }
+
+        public override void OnCapabilitiesChanged(Network network, NetworkCapabilities networkCapabilities)
+        {
+            base.OnCapabilitiesChanged(network, networkCapabilities);
+            WifiInfo wifiInfo = (WifiInfo)networkCapabilities.TransportInfo;
+
+            if (wifiInfo != null)
+            {
+                string ssid = wifiInfo.SSID.Trim(new char[] { '"', '\"' });
+                string bssid = wifiInfo.BSSID;
+            }
+        }
     }
 }
