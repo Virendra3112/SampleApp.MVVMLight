02c5f2bf935d1c5d45acdb2085a7d4423ca1f402
Virendra <virendra.thakur31@gmail.com>
2023-08-29 22:11:28 +0530

    ExtendedTabbedPageRenderer added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.Android/CustomRendrers/ExtendedTabbedPageRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+namespace SampleApp.MVVMLight.Droid.CustomRendrers
+{
+    public class ExtendedTabbedPageRenderer : TabbedPageRenderer
+    {
+        public ExtendedTabbedPageRenderer(Context context) : base(context)
+        {
+        }
+    }
+}
