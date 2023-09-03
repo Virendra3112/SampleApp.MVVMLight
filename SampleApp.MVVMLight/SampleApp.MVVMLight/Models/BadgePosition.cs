d9b51acc7b7017a96948b984d6617308dc01fef4
Virendra <virendra.thakur31@gmail.com>
2023-09-03 21:10:47 +0530

    BadgePosition added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/Models/BadgePosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.MVVMLight.Models
+{
+    public enum BadgePosition
+    {
+        PositionTopRight = 0,
+        PositionTopLeft = 1,
+        PositionBottomRight = 2,
+        PositionBottomLeft = 3,
+        PositionCenter = 4,
+        PositionTopCenter = 5,
+        PositionBottomCenter = 6,
+        PositionLeftCenter = 7,
+        PositionRightCenter = 8,
+    }
+}
