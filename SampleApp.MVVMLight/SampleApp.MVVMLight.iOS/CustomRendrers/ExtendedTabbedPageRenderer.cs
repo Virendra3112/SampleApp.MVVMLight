99ef9d192b424ea593655f7731ea3a6b00daa55f
Virendra <virendra.thakur31@gmail.com>
2023-09-11 21:53:44 +0530

    ExtendedTabbedPageRenderer for iOS.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight.iOS/CustomRendrers/ExtendedTabbedPageRenderer.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using SampleApp.MVVMLight.CustomControls;
+using SampleApp.MVVMLight.iOS.CustomRendrers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedTabbedPage), typeof(ExtendedTabbedPageRenderer))]
+namespace SampleApp.MVVMLight.iOS.CustomRendrers
+{
+   public class ExtendedTabbedPageRenderer : TabbedRenderer
+    {
+    }
+}
