84fd2ab7549366badcc103b3d3271316115afb9d
Virendra <virendra.thakur31@gmail.com>
2023-09-13 20:42:31 +0530

    CustomTabbedPageSampleTwoViewModel added.

---
+++ SampleApp.MVVMLight/SampleApp.MVVMLight/ViewModels/CustomTabbedPageSampleTwoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.MVVMLight.ViewModels
+{
+    public class CustomTabbedPageSampleTwoViewModel : BaseViewModel
+    {
+        public CustomTabbedPageSampleTwoViewModel()
+        {
+
+        }
+    }
+}
